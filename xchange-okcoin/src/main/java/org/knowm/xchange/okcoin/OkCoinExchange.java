cad7985f3c687b5d745b31a0ef9e792a55536dc6
Sergei Shurmin <slixsl007@gmail.com>
2017-06-23 08:41:28 +0200

    okcoin - change site for futures

--- xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/OkCoinExchange.java
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/OkCoinExchange.java
@@ -52,11 +52,16 @@ public class OkCoinExchange extends BaseExchange {
   /** Adjust host parameters depending on exchange specific parameters */
   private static void concludeHostParams(ExchangeSpecification exchangeSpecification) {
 
-    if (exchangeSpecification.getExchangeSpecificParameters() != null
-        && exchangeSpecification.getExchangeSpecificParametersItem("Use_Intl").equals(true)) {
-      exchangeSpecification.setSslUri("https://www.okcoin.com/api");
-      exchangeSpecification.setHost("www.okcoin.com");
-      exchangeSpecification.setExchangeSpecificParametersItem("Websocket_SslUri", "wss://real.okcoin.com:10440/websocket/okcoinapi");
+    if (exchangeSpecification.getExchangeSpecificParameters() != null) {
+        if (exchangeSpecification.getExchangeSpecificParametersItem("Use_Futures").equals(true)) {
+            exchangeSpecification.setSslUri("https://www.okex.com/api");
+            exchangeSpecification.setHost("www.okex.com");
+            exchangeSpecification.setExchangeSpecificParametersItem("Websocket_SslUri", "wss://real.okex.com:10440/websocket/okcoinapi");
+        } else if (exchangeSpecification.getExchangeSpecificParametersItem("Use_Intl").equals(true)) {
+            exchangeSpecification.setSslUri("https://www.okcoin.com/api");
+            exchangeSpecification.setHost("www.okcoin.com");
+            exchangeSpecification.setExchangeSpecificParametersItem("Websocket_SslUri", "wss://real.okcoin.com:10440/websocket/okcoinapi");
+        }
     }
   }
 
